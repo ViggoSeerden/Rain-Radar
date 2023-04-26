68a336d0e058f1f3cad90a78a6a95f12c2279d3e
SteijnPloegmakers <steijn@familieploegmakers.nl>
2023-04-26 12:57:36 +0200

    Made music visible with MusicKit

--- RainRadar/ContentView.swift
+++ RainRadar/ContentView.swift
@@ -6,12 +6,39 @@
 //
 
 import SwiftUI
+import MusicKit
 
 struct ContentView: View {
     @ObservedObject var weatherKitManager = WeatherKitManager()
     @StateObject var locationDataManager = LocationDataManager()
     
+    struct Item: Identifiable, Hashable {
+        var id = UUID()
+        let name: String
+        let artist: String
+        let imageUrl: URL?
+    }
+    
+    @State var songs = [Item]()
+    
     var body: some View {
+        NavigationStack{
+            List(songs) {
+                song in HStack{
+                    AsyncImage(url: song.imageUrl)
+                        .frame(width: 75, height: 75, alignment: .center)
+                    VStack(alignment: .leading){
+                        Text(song.name)
+                            .font(.title3)
+                        Text(song.artist)
+                            .font(.footnote)
+                    }.padding()
+                }
+            }
+        }.onAppear(){
+            fetchMusic()
+        }
+        
         if locationDataManager.authorizationStatus == .authorizedWhenInUse {
             VStack {
                 Text(weatherKitManager.text)
@@ -20,8 +47,8 @@ struct ContentView: View {
                         await weatherKitManager.getWeather(latitude: locationDataManager.latitude, longitude: locationDataManager.longitude)
                         await weatherKitManager.getHourlyForecast(latitude: locationDataManager.latitude, longitude: locationDataManager.longitude)
                     }
-                
-                if let hourlyWeather = weatherKitManager.hourlyForecast {
+
+                /*if let hourlyWeather = weatherKitManager.hourlyForecast {
                     ScrollView {
                         ForEach(hourlyWeather, id: \.self.date) { weatherEntry in
                             HStack {
@@ -33,11 +60,41 @@ struct ContentView: View {
                         }
                     }
                     .frame(width: 300, height: 600)
-                }
+                }*/
             }
         }
         else {
             Text("Error Loading Location:")
         }
     }
+    private let request: MusicCatalogSearchRequest = {
+        var request = MusicCatalogSearchRequest(term: "Flowers", types: [Song.self])
+        request.limit = 10
+        return request
+    }()
+    
+    private func fetchMusic(){
+        Task{
+            //Request Permission
+            let status = await MusicAuthorization.request()
+            switch status{
+            case .authorized:
+                //Request -> Response
+                do{
+                    let result = try await request.response()
+                    self.songs = result.songs.compactMap({
+                        return .init(name: $0.title, artist:$0.artistName, imageUrl: $0.artwork?.url(width: 75, height: 75))
+                    })
+                    print(String(describing: songs[0]))
+                }catch{
+                    print(String(describing: error))
+                }
+                //Assign Song
+                
+            default:
+                break
+            }
+
+            }
+        }
 }
---
+++ RainRadar/MusicManager/MusicKitManager.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  RainRadar
+//
+//  Created by Steijn on 26/04/2023.
+//
+
+import Foundation
+import MusicKit
+
+class MusicKitManager: ObservableObject {
+    
+}
