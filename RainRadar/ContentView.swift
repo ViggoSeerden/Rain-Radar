8f5bb8f82565f5c698358098851cc4e38b12c13b
Viggo <491216@student.fontys.nl>
2023-04-26 13:09:48 +0200

    Hourly Forecast Tabs Swiping Update

--- RainRadar/ContentView.swift
+++ RainRadar/ContentView.swift
@@ -6,10 +6,20 @@
 //
 
 import SwiftUI
+import WeatherKit
 
 struct ContentView: View {
     @ObservedObject var weatherKitManager = WeatherKitManager()
     @StateObject var locationDataManager = LocationDataManager()
+    
+    var hourlyForecastByDate: [Date: [HourWeather]] {
+        guard let hourlyForecast = weatherKitManager.hourlyForecast else { return [:] }
+        return Dictionary(grouping: hourlyForecast.dropFirst(11), by: { Calendar.current.date(bySettingHour: 0, minute: 0, second: 0, of: $0.date)! })
+    }
+    
+    var sortedDates: [Date] {
+        hourlyForecastByDate.keys.sorted()
+    }
     
     var body: some View {
         if locationDataManager.authorizationStatus == .authorizedWhenInUse {
@@ -21,18 +31,26 @@ struct ContentView: View {
                         await weatherKitManager.getHourlyForecast(latitude: locationDataManager.latitude, longitude: locationDataManager.longitude)
                     }
                 
-                if let hourlyWeather = weatherKitManager.hourlyForecast {
-                    ScrollView {
-                        ForEach(hourlyWeather, id: \.self.date) { weatherEntry in
-                            HStack {
-                                Text(DateFormatter.localizedString(from: weatherEntry.date, dateStyle: .short, timeStyle: .short))
-                                Spacer()
-                                Image(systemName: weatherEntry.symbolName)
-                                Text(weatherKitManager.convertTemp(temperature: weatherEntry.temperature))
+                if !hourlyForecastByDate.isEmpty {
+                    TabView {
+                        ForEach(sortedDates, id: \.self) { date in
+                            if let weatherEntries = hourlyForecastByDate[date] {
+                                ScrollView {
+                                    ForEach(weatherEntries, id: \.self.date) { weatherEntry in
+                                        HStack {
+                                            Text(DateFormatter.localizedString(from: weatherEntry.date, dateStyle: .short, timeStyle: .short))
+                                            Spacer()
+                                            Image(systemName: weatherEntry.symbolName)
+                                            Text(weatherKitManager.convertTemp(temperature: weatherEntry.temperature))
+                                        }
+                                    }
+                                }
+                                .frame(width: 300, height: 600)
+                                .tabItem { Text(DateFormatter.localizedString(from: date, dateStyle: .short, timeStyle: .short)) }
                             }
                         }
                     }
-                    .frame(width: 300, height: 600)
+                    .tabViewStyle(PageTabViewStyle(indexDisplayMode: .always))
                 }
             }
         }
